86581bdeb667898d529f2bee453ef5f421f9d412
Beata <B.Fonferko-Shadrach@swansea.ac.uk>
2021-10-07 16:01:20 +0100

    reloaded 2 groovy files, clinic and letter date

---
+++ Groovy/ClinicDate.groovy
@@ -0,0 +1,10 @@
+new File(scriptParams.outputFile).withWriterAppend{ out ->
+  doc.getAnnotations("Output").get("ClinicDate").each{
+    anno ->
+      def f = anno.getFeatures()
+      String[] id =  doc.getFeatures().get("gate.SourceURL").split("/")
+      out.writeLine(/${id[-1]},${anno.start()},${anno.end()}, ${f.get('Date')},/+
+      /"${f.get('DayDate')}",${f.get('MonthDate')}, ${f.get('YearDate')},/+
+      /${f.get('value')},${f.get('rule')}/)
+    }
+  }
